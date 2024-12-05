dc063ddcd183dd2bdddb52585fa0d46f09943cd1
Ross Piper <rp-webservices@outlook.com>
2024-12-05 07:39:28 +0000

    Quiz 1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -12,6 +12,14 @@
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
+fn calculate_price_of_apples(num_apples: i32) -> i32 {
+    if num_apples > 40 {
+        num_apples
+    } else {
+        num_apples * 2
+    }
+}
+
 fn main() {
     // You can optionally experiment here.
 }
