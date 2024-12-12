1476f385234eb1f87aa692bdb49002cfe76664b1
Ross Piper <rp-webservices@outlook.com>
2024-12-12 07:34:58 +0000

    Quiz 2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -28,6 +28,20 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = Vec::new();
+        for (string, command) in input {
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(usize) => {
+                    let suffix = "bar".repeat(usize);
+                    output.push(string + &suffix);
+                }
+            };
+        }
+        output
+    }
 }
 
 fn main() {
@@ -38,6 +52,7 @@ fn main() {
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
